108b371779ec7804c4599fd53af7862b6cbb8cc7
Arnaud Bergeron <abergeron@gmail.com>
2020-06-17 18:35:42 +0000

    Add stub class

--- src/relay/backend/contrib/poplar/codegen.cc
+++ src/relay/backend/contrib/poplar/codegen.cc
@@ -7,6 +7,28 @@
 namespace tvm {
 namespace relay {
 namespace contrib {
+
+class PoplarCodeGen : public ExprVisitor {
+public:
+  explicit PoplarCodeGen();
+
+  void VisitExpr_(const VarNode* node) {}
+  void VisitExpr_(const GlobalVarNode* node) {}
+  void VisitExpr_(const ConstantNode* node) {}
+  void VisitExpr_(const TupleNode* node) {}
+  void VisitExpr_(const FunctionNode* node) {}
+  void VisitExpr_(const CallNode* node) {}
+  void VisitExpr_(const LetNode* node) {}
+  void VisitExpr_(const IfNode* node) {}
+  void VisitExpr_(const OpNode* node) {}
+  void VisitExpr_(const TupleGetItemNode* node) {}
+  void VisitExpr_(const RefCreateNode* node) {}
+  void VisitExpr_(const RefReadNode* node) {}
+  void VisitExpr_(const RefWriteNode* node) {}
+  void VisitExpr_(const ConstructorNode* node) {}
+  void VisitExpr_(const MatchNode* node) {}
+}
+
 }
 }
 }
