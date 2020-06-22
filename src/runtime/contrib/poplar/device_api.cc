ef1353b89cd4981b9d0037bfab5ebc112d93a540
Arnaud Bergeron <abergeron@gmail.com>
2020-06-22 21:32:55 +0000

    Hopefully working device API for IPU (but we might not need this)

--- src/runtime/contrib/poplar/device_api.cc
+++ src/runtime/contrib/poplar/device_api.cc
@@ -14,19 +14,19 @@ class IPUThreadEntry;
 class IPUDeviceAPI final : public DeviceAPI {
 public:
   void SetDevice(TVMContext ctx) final;
-  void GetAttr(TVMContext ctx, DeviceAttrKind kind, TVMRetValue *rv) final;
-  void *AllocDataSpace(TVMContext ctx, size_t nbytes, size_t alignment,
-		       DLDataType type_hint) final { return NULL; }
-  void FreeDataSpace(TVMContext ctx, void *ptr) final { }
+  void GetAttr(TVMContext ctx, DeviceAttrKind kind, TVMRetValue* rv) final;
+  void* AllocDataSpace(TVMContext ctx, size_t nbytes, size_t alignment,
+		       DLDataType type_hint) final;
+  void FreeDataSpace(TVMContext ctx, void* ptr) final;
   void CopyDataFromTo(const void* from, size_t from_offset,
 		      void* to, size_t to_offset, size_t size,
 		      TVMContext ctx_from, TVMContext ctx_to,
-		      DLDataType type_hint, TVMStreamHandle stream) final { }
+		      DLDataType type_hint, TVMStreamHandle stream) final;
   void StreamSync(TVMContext ctx, TVMStreamHandle stream) final {}
-  void* AllocWorkspace(TVMContext ctx, size_t size, DLDataType type_hint) final{ return NULL; }
-  void FreeWorkspace(TVMContext ctx, void* data) final { }
 
   IPUThreadEntry* GetThreadEntry();
+
+  IPUDeviceAPI() : m_(poplar::DeviceManager::createDeviceManager()) {}
 
   static const std::shared_ptr<IPUDeviceAPI>& Global() {
     static std::shared_ptr<IPUDeviceAPI> inst = std::make_shared<IPUDeviceAPI>();
@@ -87,6 +87,29 @@ void IPUDeviceAPI::GetAttr(TVMContext ctx, DeviceAttrKind kind, TVMRetValue* rv)
       return;
   }
 }
+
+void* IPUDeviceAPI::AllocDataSpace(TVMContext ctx, size_t nbytes, size_t alignment,
+				   DLDataType type_hint) {
+  // We allocate the buffers on the CPU since it's not really possible
+  // to allocate on the device. We ignore alignment since this is not
+  // the location that will be referenced for execution.
+  void* ptr;
+  ptr = malloc(nbytes);
+  if (ptr == nullptr) throw std::bad_alloc();
+  return ptr;
+}
+
+void IPUDeviceAPI::FreeDataSpace(TVMContext ctx, void* ptr)  {
+  free(ptr);
+}
+
+void IPUDeviceAPI::CopyDataFromTo(const void* from, size_t from_offset,
+				  void* to, size_t to_offset, size_t size,
+				  TVMContext ctx_from, TVMContext ctx_to,
+				  DLDataType type_hint, TVMStreamHandle stream) {
+  memcpy(static_cast<char*>(to) + to_offset, static_cast<const char*>(from) + from_offset, size);
+}
+
 
 TVM_REGISTER_GLOBAL("device_api.ipu").set_body([](TVMArgs args, TVMRetValue* rv) {
     DeviceAPI* ptr = IPUDeviceAPI::Global().get();
