dbfefb4745738b7cf8b8267c4085e8bccee3df61
Arnaud Bergeron <abergeron@gmail.com>
2020-06-23 19:22:42 +0000

    setup argument mapping skeleton

--- src/relay/backend/contrib/poplar/codegen.cc
+++ src/relay/backend/contrib/poplar/codegen.cc
@@ -1,6 +1,8 @@
 #include <poplar/Graph.hpp>
 #include <poplar/Target.hpp>
 #include <poplar/Program.hpp>
+#include <poplar/Type.hpp>
+#include <poplar/VariableMappingMethod.hpp>
 
 #include <tvm/relay/expr_functor.h>
 #include <tvm/relay/transform.h>
@@ -11,6 +13,48 @@
 namespace tvm {
 namespace relay {
 namespace contrib {
+  /*
+static poplar::Type to_poplar_type(DataType& dt) {
+  if (dt.lanes() != 1) {
+    LOG(FATAL) << "Poplar doesn't support multi-lane data types (for now)\n";
+  }
+  switch (dt.code()) {
+  case kDLInt:
+    switch (dt.bits()) {
+    case 32:
+      return poplar::SIGNED_INT;
+    case 16:
+      return poplar::SIGNED_SHORT;
+    case 8:
+      return poplar::SIGNED_CHAR;
+    }
+    break;
+  case kDLUInt:
+    switch (dt.bits()) {
+    case 32:
+      return poplar::UNSIGNED_INT;
+    case 16:
+      return poplar::UNSIGNED_SHORT;
+    case 8:
+      return poplar::UNSIGNED_CHAR;
+    case 1:
+      return poplar::BOOL;
+    }
+    break;
+  case kDLFloat:
+    switch(dt.bits()) {
+    case 32:
+      return poplar::FLOAT;
+    case 16:
+      return poplar::HALF;
+    }
+    break;
+  default:
+    break;
+  }
+  LOG(FATAL) << "Unsupported data type for poplar:" << dt << "\n";
+}
+  */
 
 class PoplarCodeGen : public ExprVisitor {
 public:
@@ -32,35 +76,40 @@ public:
     } else if (ref->IsInstance<IRModuleNode>()) {
       IRModule mod = Downcast<IRModule>(ref);
 
-      BaseFunc main = mod->Lookup("main");
+      Function main = Downcast<Function>(mod->Lookup("main"));
+
       prog_map_[main] = 2;
       progs[2] = poplar::program::Sequence();
+      setup_args(main, 2);
 
       // First map the functions to progams
       for (const auto& it : mod->functions) {
 	// We skip the "main" function since it was handled above
+
 	if (it.first->name_hint.compare("main") == 0)
 	  continue;
 
+	Function f = Downcast<Function>(it.second);
 	size_t index = progs.size();
 	progs.push_back(poplar::program::Sequence());
-	prog_map_[it.second] = index;
+	prog_map_[f] = index;
+	setup_args(f, index);
       }
-
-      // Main is special since its input/output go to the outside world
-
 
       // Then convert the functions
       for (const auto& it : mod->functions) {
 	// We skip the "main" function since it was handled above
-	if (it.first->name_hint.compare("main") == 0)
-	  continue;
+	Function f = Downcast<Function>(it.second);
 
-	curprog_ = &progs[prog_map_[it.second]];
-	this->VisitExpr(Downcast<Function>(it.second));
+	curprog_ = &progs[prog_map_[f]];
+	this->VisitExpr(f);
 	curprog_ = nullptr;
       }
     }
+
+
+    // Make the programs to copy inputs/outputs
+    
 
     prog_map_.clear();
     progs_ = nullptr;
@@ -85,10 +134,24 @@ public:
   void VisitExpr_(const MatchNode* node) {}
 
 private:
+  void setup_args(Function fn, size_t index) {
+    if (arg_map_.size() < (index - 1))
+      arg_map_.resize(index - 1);
+    auto& args = arg_map_[index-2];
+    for (const auto& it : fn->params) {
+      args.push_back(curg_->addVariable(poplar::FLOAT, {}, poplar::VariableMappingMethod::LINEAR,
+					it->vid->name_hint.c_str()));
+    }
+    // Last is the output
+    args.push_back(curg_->addVariable(poplar::FLOAT, {}, poplar::VariableMappingMethod::LINEAR,
+				      "fn_output"));
+  }
+
   poplar::Graph* curg_;
   poplar::program::Program* curprog_;
   std::vector<poplar::program::Program>* progs_;
-  std::map<BaseFunc, size_t> prog_map_;
+  std::map<Function, size_t> prog_map_;
+  std::vector<std::vector<poplar::Tensor>> arg_map_;
 };
 
 runtime::Module PoplarCompiler(const ObjectRef& ref) {
