e89ff0bab3ff7784db207841a693846670444422
Arnaud Bergeron <abergeron@gmail.com>
2020-07-13 11:05:27 -0400

    Remove empty files

--- python/tvm/relay/op/strategy/ipu.py
+++
@@ -1,9 +0,0 @@
-import topi
-from .generic import *
-
-
-@schedule_injective.register("ipu")
-def schedule_injective_cpu(attrs, outs, target):
-    """schedule injective ops for ipu"""
-    with target:
-        return topi.ipu.schedule_injective(outs)
